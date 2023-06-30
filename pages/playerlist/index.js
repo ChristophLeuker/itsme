428a52a04422bd16bea3b4f86f6bb65430b73934
Christoph Leuker <cleuker87@gmail.com>
2023-06-30 12:24:36 +0200

    deleted useless fragments

--- pages/playerlist/index.js
+++ pages/playerlist/index.js
@@ -2,18 +2,16 @@ import useSWR from "swr";
 
 export default function PlayerList() {
   const { data = [] } = useSWR("/api/players");
-  console.log(data);
+
   return (
     <>
       <h1>Hello Players:</h1>
       <ul>
         {data.map((player) => {
           return (
-            <>
-              <li key={player._id}>
-                Name: {player.name} E-Mail:{player.email}
-              </li>
-            </>
+            <li key={player._id}>
+              Name: {player.name} E-Mail:{player.email}
+            </li>
           );
         })}
       </ul>
