2a952418c15eaa4f8d833fa65cecab6a4a42b1eb
Christoph Leuker <cleuker87@gmail.com>
2023-07-04 11:02:08 +0200

    add svg for delete and edit

--- components/profileCard/index.js
+++ components/profileCard/index.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 const Listitem = styled.li`
   display: flex;
@@ -9,11 +10,19 @@ const Listitem = styled.li`
   width: 90%;
   height: 4rem;
   margin-top: 1rem;
-
   padding: 0.7rem;
+  position: relative;
 `;
 const StyledText = styled.p`
   margin: 1px;
+`;
+
+const ImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  right: 20px;
+  gap: 5px;
 `;
 
 export default function ProfileCard({ name, email }) {
@@ -22,6 +31,20 @@ export default function ProfileCard({ name, email }) {
       <Listitem>
         <StyledText>NAME: {name}</StyledText>
         <StyledText>EMAIL: {email}</StyledText>
+        <ImageContainer>
+          <Image
+            src="/delete-button-svg.svg"
+            alt="delete svg"
+            width={20}
+            height={20}
+          />
+          <Image
+            src="/edit-button-svg.svg"
+            alt="edit svg"
+            width={20}
+            height={20}
+          />
+        </ImageContainer>
       </Listitem>
     </>
   );
