b0bf851a3195aedf64d7a8d7abc5a0422f8946fa
Christoph Leuker <cleuker87@gmail.com>
2023-07-14 09:42:00 +0200

    add vercel preview env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,52 @@
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const providers = [];
+
+if (process.env.VERCEL_ENV === "preview") {
+  providers.push(
+    // Create a credentials provider with dummy data, describing input fields:
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "player" },
+        password: { label: "Password", type: "password" },
+      },
+      // and adding a fake authorization with static username and password:
+      async authorize(credentials) {
+        if (
+          credentials.username === "chris" &&
+          credentials.password === "dart"
+        ) {
+          return {
+            id: "1",
+            name: "Chris",
+            email: "cleuker87@gmail.com",
+          };
+        } else {
+          return null;
+        }
+      },
+    })
+  );
+} else {
+  // If not on preview, we use the GithubProvider as before
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  );
+}
 
 export const authOptions = {
+  providers,
+};
+
+export default NextAuth(authOptions);
+
+/* export const authOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -9,4 +54,4 @@ export const authOptions = {
     }),
   ],
 };
-export default NextAuth(authOptions);
+export default NextAuth(authOptions); */
